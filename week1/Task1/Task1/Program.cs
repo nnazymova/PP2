402c33d48ae34ec231deda9887a548d366244bd7
nnazymova <nazymova.nazerke@gmail.com>
2019-01-31 13:03:13 +0600

    asd

---
+++ week1/Task1/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        string b;
+        b = Console.ReadLine();
+        int n = int.Parse(b);
+        
+        string [] arr = Console.ReadLine().Split();
+
+        int sum = 0;
+        for(int i = 0; i < n; i++)
+        {
+            int c = int.Parse(arr[i]);
+            int j;
+            for (j = 2; j < c; j++)
+                if ((c % j == 0))
+                {
+                    break;
+                }
+
+
+            if (j == c)
+            {
+                Console.Write(c + " ");
+                sum++;
+            }
+        }
+
+
+    }
+}
---
+++ week1/Task2/Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
