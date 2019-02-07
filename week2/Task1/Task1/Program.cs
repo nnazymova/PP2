6f342516439ea01407e487c8999f5446cb90c815
nnazymova <nazymova.nazerke@gmail.com>
2019-02-07 17:42:02 +0600

    a

---
+++ week2/Task1/Task1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Task1
+{
+    class MainClass
+    {
+        //создаем функцию, которая возвращает true, false после выполнения условия 
+        public static bool P(string str)
+        {
+            //объявляем переменные: начало слово и конец
+            int i = 0;
+            int j = str.Length - 1;
+
+            //условие: пока i меньше j(пока отсчет дойдет до середины)
+            while (i < j)
+            {
+                //если значение i-того и j-того элемента не равны, возвращать false
+                if (str[i] != str[j])
+                    return false;
+                //увеличиваем значение i и уменьшаем j, чтобы проверить полностью слово
+                i++;
+                j--;
+            }
+
+            //если указанное выше условие не выполняется, возвращаем true
+            return true;
+        }
+
+        public static void Main(string[] args)
+        {
+            //считываем с кансоли слово
+            string s = Console.ReadLine();
+
+            //присваиваем новой переменной функцию и передаем туда считанное слово
+            bool a = P(s);
+
+            //если условие выполнено, выводим "YES"
+            if(a == true)
+            {
+                Console.WriteLine("Yes");
+            }
+            //в противном случае выводим "NO"
+            else
+            {
+                Console.WriteLine("NO");
+
+            }
+
+        }
+    }
+}
---
+++ week2/Task2/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+                string t = System.IO.File.ReadAllText(@"C:\Users\Desktop\fortask2\text.txt");
+                string [] txt = t.Split();
+
+                for (int i = 0; i < txt.Length; i++)
+                {
+                    int c = int.Parse(txt[i]);
+                    int j;
+                    for (j = 2; j < c; j++)
+                        if ((c % j == 0))
+                        {
+                            break;
+                        }
+
+
+                    if (j == c)
+                    {
+                        System.IO.File.WriteAllText(@"C:\Users\Desktop\fortask2\output.txt", txt[i]);
+                    }
+                }
+            }
+    }
+}
